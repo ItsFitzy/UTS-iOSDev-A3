125134c32d4fc1ff7e5c0800b6e1984279317c99
ItsFitzy <ethan@fitzgeralds.id.au>
2022-05-01 12:17:34 +1000

    Created Blank Dinecraft Project

---
+++ Dinecraft/Dinecraft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dinecraft
+//
+//  Created by Ethan Fitzgerald on 1/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
