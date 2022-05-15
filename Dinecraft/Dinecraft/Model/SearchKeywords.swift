c4442de904ec42c1ad46f2ddbf358fd8923e11e3
ItsFitzy <ethan@fitzgeralds.id.au>
2022-05-15 23:01:54 +1000

    Added Fruit Search Keywords
    - Added the Model file group (idk why it's not just a folder)
    - Added the SearchKeywords Swift file
    - Added the Keywords class to handle storing the search keywords and giving a random one

---
+++ Dinecraft/Dinecraft/Model/SearchKeywords.swift
@@ -0,0 +1,87 @@
+//
+//  SearchKeywords.swift
+//  Dinecraft
+//
+//  Created by Ethan Fitzgerald on 15/5/2022.
+//
+
+import Foundation
+
+class Keywords {
+    //Properties
+    
+    
+    //Variables
+    
+    
+    //References
+    
+    
+    //Data
+    let fruits = [
+      "Apple",
+      "Apricot",
+      "Avocado",
+      "Banana",
+      "Berries",
+      "Breadfruit",
+      "Carob",
+      "Cherry",
+      "Citron",
+      "Coconut",
+      "Date",
+      "Dragon Fruit",
+      "Durian",
+      "Fig",
+      "Ginger",
+      "Grapes",
+      "Currant",
+      "Raisin",
+      "Grapefruit",
+      "Guava",
+      "Jackfruit",
+      "Jujube",
+      "Kiwifruit",
+      "Kumquat",
+      "Lemon",
+      "Lime",
+      "Longan",
+      "Loquat",
+      "Lucuma",
+      "Lychee",
+      "Mamey Sapote",
+      "Mango",
+      "Mangosteen",
+      "Melons",
+      "Nance",
+      "Nectarine",
+      "Noni",
+      "Oranges",
+      "Mandarin",
+      "Navel",
+      "Seville",
+      "Valencia",
+      "Papaya",
+      "Passion Fruit",
+      "Peach",
+      "Pear",
+      "Asian Pear",
+      "Persimmon",
+      "Pineapple",
+      "Plantain",
+      "Plum",
+      "Damson",
+      "Prunes",
+      "Pomegranate",
+      "Pomelo",
+      "Quince",
+      "Rambutan",
+      "Rhubarb",
+      "Starfruit",
+      "Tamarillo",
+      "Tamarind",
+      "Tangerine",
+      "Tangelo",
+      "Tomato",
+    ]
+}
