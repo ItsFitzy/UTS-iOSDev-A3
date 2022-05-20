df4f5937fdffc8c2c05a4a7ef7f96835892e0021
JokerMain668 <44485309+JokerMain668@users.noreply.github.com>
2022-05-21 02:40:12 +1000

    Fixed ScrollView

--- Dinecraft/Dinecraft/ViewControllers/DietViewController.swift
+++ Dinecraft/Dinecraft/ViewControllers/DietViewController.swift
@@ -9,8 +9,22 @@ import UIKit
 
 class DietViewController: UIViewController
 {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet var LblGrp: [UILabel]!
     var allergiesData: [Bool] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: 1200)
+        for Lbl in LblGrp {
+            Lbl.center.x = -100
+            Lbl.alpha = 0
+        }
+        for i in 0...19
+        {
+            UIView.animate(withDuration: 1.0, delay: 0.05 * Double(i), usingSpringWithDamping: 1.0, initialSpringVelocity: 5.0, options: [], animations: ({self.LblGrp[i].center.x = 123
+                self.LblGrp[i].alpha = 1
+            }), completion: nil)
+        }
     }
 }
