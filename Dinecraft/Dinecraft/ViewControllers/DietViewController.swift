2081ec87864882ee61b23bae7cbbc23c8c4f880d
JokerMain668 <44485309+JokerMain668@users.noreply.github.com>
2022-05-20 23:31:17 +1000

    Push for ethan

---
+++ Dinecraft/Dinecraft/ViewControllers/DietViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DietViewController.swift
+//  Dinecraft
+//
+//  Created by Bennett Chung on 20/5/2022.
+//
+
+import UIKit
+
+class DietViewController: UIViewController
+{
+    var allergiesData: [Bool] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
